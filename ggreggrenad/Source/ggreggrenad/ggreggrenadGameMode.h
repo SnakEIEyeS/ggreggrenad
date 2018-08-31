533250f45f9b6e17c612741841ea0f4bd022f12e
Shantanu Mane <snake_eyes0017@yahoo.com>
2018-08-31 13:27:39 -0600

    Created project. Added BP GameMode

---
+++ ggreggrenad/Source/ggreggrenad/ggreggrenadGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ggreggrenadGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AggreggrenadGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AggreggrenadGameMode();
+};
+
+
+
