e6b1941b5f6b708d402719ffd0e3862b152f955a
Shantanu Mane <snake_eyes0017@yahoo.com>
2018-09-04 11:46:06 -0600

    Restricted player movement, added net & map, using fixed camera position

--- ggreggrenad/Source/ggreggrenad/ggreggrenadCharacter.cpp
+++ ggreggrenad/Source/ggreggrenad/ggreggrenadCharacter.cpp
@@ -33,15 +33,15 @@ AggreggrenadCharacter::AggreggrenadCharacter()
 	GetCharacterMovement()->AirControl = 0.2f;
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+	//CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	//CameraBoom->SetupAttachment(RootComponent);
+	//CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
+	//CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	//FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	//FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	//FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
@@ -57,7 +57,7 @@ void AggreggrenadCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &AggreggrenadCharacter::MoveForward);
+	//PlayerInputComponent->BindAxis("MoveForward", this, &AggreggrenadCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &AggreggrenadCharacter::MoveRight);
 
 	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
@@ -104,7 +104,7 @@ void AggreggrenadCharacter::LookUpAtRate(float Rate)
 	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
-void AggreggrenadCharacter::MoveForward(float Value)
+void AggreggrenadCharacter::MoveRight(float Value)
 {
 	if ((Controller != NULL) && (Value != 0.0f))
 	{
@@ -118,7 +118,7 @@ void AggreggrenadCharacter::MoveForward(float Value)
 	}
 }
 
-void AggreggrenadCharacter::MoveRight(float Value)
+/*void AggreggrenadCharacter::MoveRight_TP(float Value)
 {
 	if ( (Controller != NULL) && (Value != 0.0f) )
 	{
@@ -131,4 +131,4 @@ void AggreggrenadCharacter::MoveRight(float Value)
 		// add movement in that direction
 		AddMovementInput(Direction, Value);
 	}
-}
+}*/
